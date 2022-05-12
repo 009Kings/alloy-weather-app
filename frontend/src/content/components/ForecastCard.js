80316c99459dce9a565958e6d8838348cab8275b
009kings <009kings@gmail.com>
2022-05-11 23:03:13 -0700

    Adding a button and making things look nice

--- frontend/src/content/components/ForecastCard.js
+++ frontend/src/content/components/ForecastCard.js
@@ -2,13 +2,15 @@ const ForecastCard = (props) => {
   // TODO: Store preference in Local storage
   // K => F   (K - 273.15) × 9/5 + 32
   // K => C   K - 273.15
-  let date = new Date(props.dt * 1000)
 
+  let date = new Date(props.dt * 1000)
   let toF = k => ((k - 273.15) * 9/5 + 32)
+
+  // How would you know if it was already faved? 
 
   return (
     <div className="forecast-card">
-      <h1>
+      <h3>
         {date.toLocaleDateString(
           'en-gb',
           {
@@ -16,11 +18,12 @@ const ForecastCard = (props) => {
             month: 'long',
             day: 'numeric'
           }
-        )}
-      </h1>
-      <h3>{props.desc}</h3>
+          )}
+      </h3>
+      <h2>{props.desc[0].toUpperCase() + props.desc.slice(1)}</h2>
       <h4>High of {Math.round(toF(props.high))}°F</h4>
       <h5>Low of {Math.round(toF(props.low))}°F</h5>
+      <button className="fave"><i class="fa-regular fa-heart"></i></button>
     </div>
   );
 }
